af76d5bc089a339f441b132652dd86e940b733d7
Nikita <svstnnk@gmail.com>
2024-10-25 16:29:58 +0400

    Update lessons.js with new lesson data

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        "id": 2001,
+        "subject": "Math",
+        "location": "London",
+        "price": 100.00,
+        "image": "",
+        "availableInventory": 5
+       
+    },
+    {
+        "id": 2002,
+        "subject": "Math",
+        "location": "Oxford",
+        "price": 90.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2003,
+        "subject": "Math",
+        "location": "Liverpool",
+        "price": 80.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2004,
+        "subject": "Math",
+        "location": "Bristol",
+        "price": 120.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2005,
+        "subject": "English",
+        "location": "London",
+        "price": 100.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2006,
+        "subject": "English",
+        "location": "York",
+        "price": 80.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2007,
+        "subject": "Music",
+        "location": "Bristol",
+        "price": 90.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2008,
+        "subject": "Science",
+        "location": "Cambridge",
+        "price": 110.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2009,
+        "subject": "History",
+        "location": "Oxford",
+        "price": 95.00,
+        "image": "",
+        "availableInventory": 5
+        
+    },
+    {
+        "id": 2010,
+        "subject": "Art",
+        "location": "Liverpool",
+        "price": 85.00,
+        "image": "",
+        "availableInventory": 5
+        
+    }
+];
